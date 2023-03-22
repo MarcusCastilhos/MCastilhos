0ec2c34bb6bf0125adc797d6e0bdfc95d87b8254
Marcus Castilhos <marcus.drummer@live.com>
2023-03-22 12:51:59 -0300

    main

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
-const sequelize = require('./db.js');
-
 (async () => {
+
+  const database = require('./db.js');
+  const Produto = require('./produto')
+
+  await database.sync()
+
   try {
-    await sequelize.authenticate();
+    await database.authenticate();
     console.log('Conexão estabelecida com sucesso.');
   } catch (error) {
     console.error('Erro ao conectar ao banco de dados:', error);
   }
+
+  // const novoProduto = await Produto.create({
+  //   nome: 'Mouse USB',
+  //   preco: 15,
+  //   descricao: 'Informática'
+  // })
+
+
+  // const produto = await Produto.findAll()
+  // console.log(produto);
 })();
--- src/db.js
+++ src/db.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize')
 
 const sequelize = new Sequelize('postgres', 'postgres', 'postgres', {
-    host: 'localhost',
     dialect: 'postgres',
-    port: 5432,
+    host: 'localhost',
+    port: 5432
 })
 
 module.exports = sequelize
